eb0478cf54dea48ab54550c6f8071f519a241baa
Nishant-Pawar <pawar.nishant05@gmail.com>
2022-03-23 13:24:00 +0530

    Personal Portfolio

---
+++ scripts.js
@@ -0,0 +1,36 @@
+//function expression to select elements
+const selectElement = (s) => document.querySelector(s);
+//Open the menu on click
+selectElement('.open').addEventListener('click', () =>{
+   selectElement( '.nav-list').classList.add('active');
+});
+//Close the menu on click
+selectElement('.close').addEventListener('click', () =>{
+    selectElement('.nav-list').classList.remove('active');
+});
+//Close the menu on clicking the nav-link
+selectElement('.nav-list').addEventListener('click', () =>{
+    selectElement('.nav-list').classList.remove('active');
+});
+
+//Animate on scroll
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 },600);
+        return false;
+    });
+
+    AOS.init({
+        easing:'ease',
+        duration:1800,
+        once:true
+    })
+});
+
